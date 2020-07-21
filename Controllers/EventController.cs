47046ed3371f11d21ad652cf515597497b278f09
Ciro-Lopes <ciro.flopes@gmail.com>
2020-07-21 02:03:13 -0300

    update Event and delete Event done

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -49,5 +49,72 @@ namespace _net_core_api.Controllers
                 return this.StatusCode(StatusCodes.Status500InternalServerError, "Failed to request on database");
             }
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Event model)
+        {
+            try
+            {
+                this.Context.Events.Add(model);
+                await this.Context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Failed to request on database");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Event model, int id)
+        {
+            try
+            {
+                var entity = await this.Context.Events.FirstOrDefaultAsync(x => x.EventId == id);
+
+                if (entity != null)
+                {
+                    entity.ImageEvent = model.ImageEvent;
+                    entity.NameEvent = model.NameEvent;
+                    entity.AddresEvent = model.AddresEvent;
+                    entity.DescriptionEvent = model.DescriptionEvent;
+                    entity.TypeEvent = model.TypeEvent;
+
+                    await this.Context.SaveChangesAsync();
+
+                    return Ok(entity);
+                }
+
+                return Ok("Event not found");
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Failed to request on database");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var entity = await this.Context.Events.FirstOrDefaultAsync(x => x.EventId == id);
+
+                if (entity != null)
+                {
+                    var results = this.Context.Remove(entity);
+
+                    await this.Context.SaveChangesAsync();
+
+                    return Ok("Deleted with success");
+                }
+
+                return Ok("Event not found!");
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Failed to request on database");
+            }
+        }
     }
 }
